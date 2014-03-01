850a2dc7ef9b7995b0611b8d7e104864308772c1
JeriBeth Stevens <jeribeth@gmail.com>
2014-02-28 21:00:48 -0700

    Added keydown function
    Working functionally. Mouseenter and mouseleave conflict on keydown.

--- js/app.js
+++ js/app.js
@@ -8,18 +8,66 @@ $(document).ready(function() {
 		$(".ryu-still").show();
 	});
 	$(".ryu").mousedown(function(){
-		//play hadouken sound
-
+		playHadouken();
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
-		$(".hadouken").show();
-
-		//animate to right of screen
+		$(".hadouken").finish().show()
+			.animate(
+  			{'left': '300px'},
+  			500,
+  			function() {
+    		$(this).hide();
+    		$(this).css('left', '-212px');
+  		});
 
 	});
+
 	$(".ryu").mouseup(function(){
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
+	});
+
+	$(document).keydown(function(keyEvent) {
+		if(keyEvent.keyCode == 88) {
+			
+			$(".ryu-cool").show();
+			$(".ryu-ready").hide();
+			$(".ryu-still").hide();
+			$(".ryu-throwing").hide();
+			// xpressed = true;
+		}  
+		else {
+
+			$(".ryu-cool").hide();
+			$(".ryu-ready").hide();
+			$(".ryu-still").show();
+			$(".ryu-throwing").hide();
+
+		};
 
 	});
+
+
+
+
 });
+
+// var xpressed = false;
+
+// function throwIt() {
+// 	if (xpressed) {
+// 		$(".ryu-ready").show();
+// 	} else {
+// 		$(".ryu-still").show();
+// 	}
+// }
+
+function playHadouken () {
+	$("#hadouken-sound")[0].volume = 0.5;
+	$("#hadouken-sound")[0].load();
+	$("#hadouken-sound")[0].play();
+	
+};
+
+
+
